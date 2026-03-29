16d82c3ba27efdc9cd777eeff4b677e116ec52fc
romainvacher-droid <romainvacher@gmail.com>
2026-03-29 10:00:15 +0200

    Initial commit: Jardiner Simplement - base Streamlit + Heroku

---
+++ app_streamlit.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Jardiner Simplement",
+    page_icon="🌱",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+st.title("🌱 Jardiner Simplement")
+st.subheader("Astuces et conseils pour un jardin facile et naturel")
+
+st.markdown("---")
+
+st.info("Bienvenue ! Ce site est en cours de construction. Revenez bientôt pour découvrir nos astuces de jardinage.")
